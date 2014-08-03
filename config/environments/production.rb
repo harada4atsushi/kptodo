515212ba446806e55cbfa433110f2016db92afc5
a.harada <redhornet96@gmail.com>
2014-08-03 18:48:04 +0900

    heroku対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # TODO 環境共通？
+  Devise.setup do |config|
+    config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], :scope => 'email'
+  end
 end
