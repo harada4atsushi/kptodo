9c9aeef526d922c809859fddd4f7b4a2cf825ec3
a.harada <redhornet96@gmail.com>
2014-08-28 10:30:48 +0900

    productionの設定ファイルのsyntaxエラーを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,3 +98,4 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+end
